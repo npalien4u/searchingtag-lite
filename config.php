c10fff80883b2c47e165d6a37e5980b7d4fae114
npalien4u <npalien4u@gmail.com>
2022-06-02 13:54:07 +0530

    - installation identity-reset completed

--- config.php
+++ config.php
@@ -1,24 +1,17 @@
 <?php
-// +------------------------------------------------------------------------+
-// | @author Deen Doughouz (DoughouzForest)
-// | @author_url 1: http://www.wowonder.com
-// | @author_url 2: http://codecanyon.net/user/doughouzforest
-// | @author_email: wowondersocial@gmail.com   
-// +------------------------------------------------------------------------+
-// | WoWonder - The Ultimate PHP Social Networking Platform
-// | Copyright (c) 2017 WoWonder. All rights reserved.
-// +------------------------------------------------------------------------+
-@header('Location: install');
-exit();
+
 // MySQL Hostname
-$sql_db_host = "db_name";
+$sql_db_host = "localhost";
 // MySQL Database User
-$sql_db_user = "db_user";
+$sql_db_user = "root";
 // MySQL Database Password
-$sql_db_pass = "db_pass";
+$sql_db_pass = "";
 // MySQL Database Name
-$sql_db_name = "db_name";
+$sql_db_name = "searchingtag_lite_1";
 
 // Site URL
-$site_url = "site_url"; // e.g (http://example.com)
+$site_url = "http://searchingtag-lite.lab"; // e.g (http://example.com)
+
+// Purchase code
+$purchase_code = "1234567890"; // Your purchase code, don't give it to anyone.
 ?>
